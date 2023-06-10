b516331be4e908c5d9241237938891845125ca6c
Tao Boli <boli.tao@outlook.com>
2023-06-10 15:19:54 +0800

    feat: hero list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {Hero} from "../hero";
+import {HEROES} from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
@@ -16,5 +17,14 @@ export class HeroesComponent implements OnInit {
   }
 
   // hero = "Windstorm";
-  hero: Hero = {id: 1, name: "Windstorm"}
+  // hero: Hero = {id: 1, name: "Windstorm"}
+
+  heroes: Hero[] = HEROES;
+
+  // selectedHero?: Hero = undefined;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
