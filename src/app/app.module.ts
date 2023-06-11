1567996b063210c19e3e7c4563bc1336151d6407
Tao Boli <boli.tao@outlook.com>
2023-06-11 22:47:12 +0800

    tmp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,10 @@ import {FormsModule} from "@angular/forms";
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 import {MessagesComponent} from './messages/messages.component';
 import {AppRoutingModule} from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HttpClientModule} from "@angular/common/http";
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -17,10 +20,16 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
-    BrowserModule, FormsModule, AppRoutingModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule {
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,26 +1,44 @@
 import {Injectable} from '@angular/core';
-import {Observable, of} from "rxjs";
+import {catchError, Observable, of} from "rxjs";
 import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 import {MessageService} from "./message.service";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class HeroService {
-  constructor(private messageService: MessageService) {
+  private heroesUrl = 'api/heroes';
+
+  constructor(private messageService: MessageService,
+              private httpClient: HttpClient) {
   }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes: Observable<Hero[]> = of(HEROES);
+    let heroes = this.httpClient.get<Hero[]>(this.heroesUrl)
+      .pipe(catchError(this.handleError<Hero[]>('getHeroes', [])));
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find(s => s.id === id)!;
     this.messageService.add(`HeroService fetched hero, id: ${id}`);
     return of(hero);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+      this.log(`${operation} failed: ${error.message}`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
